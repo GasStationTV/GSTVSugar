1853ed91041b9f0e7887f7cd6a97545301ffbee4
root <starrychloe@oliveyou.net>
2013-09-25 04:46:45 +0000

    Re-added dashboard manager. Uses custom/modules/Home/index.php.

--- custom/modules/Users/logic_hooks.php
+++ custom/modules/Users/logic_hooks.php
@@ -7,6 +7,7 @@ $hook_array = Array();
 $hook_array['after_login'] = Array(); 
 $hook_array['after_login'][] = Array(1, 'SugarFeed old feed entry remover', 'modules/SugarFeed/SugarFeedFlush.php','SugarFeedFlush', 'flushStaleEntries'); 
 $hook_array['after_relationship_add'] = Array(); 
+$hook_array['after_relationship_add'][] = Array(3, 'Force Relationship Policy', 'modules/dash_DashboardManager/logic_hooks/DashboardManager_User_Hooks.php','DashboardManager_User_Hooks', 'forceRelationshipPolicy'); 
 $hook_array['after_save'] = Array(); 
 $hook_array['after_retrieve'] = Array(); 
 
