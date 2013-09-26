033c22c723d0a7feca381974234623969a45f6e9
root <starrychloe@oliveyou.net>
2013-09-26 15:31:39 +0000

    Added workflow for Accounts and Proposals

---
+++ custom/modules/Accounts/workflow/workflow.php
@@ -0,0 +1,49 @@
+<?php
+
+include_once("include/workflow/alert_utils.php");
+include_once("include/workflow/action_utils.php");
+include_once("include/workflow/time_utils.php");
+include_once("include/workflow/trigger_utils.php");
+//BEGIN WFLOW PLUGINS
+include_once("include/workflow/custom_utils.php");
+//END WFLOW PLUGINS
+	class Accounts_workflow {
+	function process_wflow_triggers(& $focus){
+		include("custom/modules/Accounts/workflow/triggers_array.php");
+		include("custom/modules/Accounts/workflow/alerts_array.php");
+		include("custom/modules/Accounts/workflow/actions_array.php");
+		include("custom/modules/Accounts/workflow/plugins_array.php");
+		
+ if(true){ 
+ 
+
+	 //Frame Secondary 
+
+	 $secondary_array = array(); 
+	 //Secondary Triggers 
+
+	global $triggeredWorkflows;
+	if (!isset($triggeredWorkflows['1312c9f1_044b_2c0d_8a92_52426ceba146'])){
+		$triggeredWorkflows['1312c9f1_044b_2c0d_8a92_52426ceba146'] = true;
+		 process_workflow_actions($focus, $action_meta_array['Accounts0_action0']); 
+ 	$_SESSION['WORKFLOW_ALERTS'] = isset($_SESSION['WORKFLOW_ALERTS']) && is_array($_SESSION['WORKFLOW_ALERTS']) ? $_SESSION['WORKFLOW_ALERTS'] : array();
+		$_SESSION['WORKFLOW_ALERTS']['Accounts'] = isset($_SESSION['WORKFLOW_ALERTS']['Accounts']) && is_array($_SESSION['WORKFLOW_ALERTS']['Accounts']) ? $_SESSION['WORKFLOW_ALERTS']['Accounts'] : array();
+		$_SESSION['WORKFLOW_ALERTS']['Accounts'] = array_merge($_SESSION['WORKFLOW_ALERTS']['Accounts'],array ());	}
+ 
+
+	 //End Frame Secondary 
+
+	 unset($secondary_array); 
+ 
+
+ //End if trigger is true 
+ } 
+
+
+	//end function process_wflow_triggers
+	}
+
+	//end class
+	}
+
+?>
---
+++ custom/modules/props_Proposals/workflow/workflow.php
@@ -0,0 +1,49 @@
+<?php
+
+include_once("include/workflow/alert_utils.php");
+include_once("include/workflow/action_utils.php");
+include_once("include/workflow/time_utils.php");
+include_once("include/workflow/trigger_utils.php");
+//BEGIN WFLOW PLUGINS
+include_once("include/workflow/custom_utils.php");
+//END WFLOW PLUGINS
+	class props_Proposals_workflow {
+	function process_wflow_triggers(& $focus){
+		include("custom/modules/props_Proposals/workflow/triggers_array.php");
+		include("custom/modules/props_Proposals/workflow/alerts_array.php");
+		include("custom/modules/props_Proposals/workflow/actions_array.php");
+		include("custom/modules/props_Proposals/workflow/plugins_array.php");
+		
+ if(true){ 
+ 
+
+	 //Frame Secondary 
+
+	 $secondary_array = array(); 
+	 //Secondary Triggers 
+
+	global $triggeredWorkflows;
+	if (!isset($triggeredWorkflows['13bd1988_d29c_1026_55cf_52426d06a806'])){
+		$triggeredWorkflows['13bd1988_d29c_1026_55cf_52426d06a806'] = true;
+		 process_workflow_actions($focus, $action_meta_array['props_Proposals0_action0']); 
+ 	$_SESSION['WORKFLOW_ALERTS'] = isset($_SESSION['WORKFLOW_ALERTS']) && is_array($_SESSION['WORKFLOW_ALERTS']) ? $_SESSION['WORKFLOW_ALERTS'] : array();
+		$_SESSION['WORKFLOW_ALERTS']['props_Proposals'] = isset($_SESSION['WORKFLOW_ALERTS']['props_Proposals']) && is_array($_SESSION['WORKFLOW_ALERTS']['props_Proposals']) ? $_SESSION['WORKFLOW_ALERTS']['props_Proposals'] : array();
+		$_SESSION['WORKFLOW_ALERTS']['props_Proposals'] = array_merge($_SESSION['WORKFLOW_ALERTS']['props_Proposals'],array ());	}
+ 
+
+	 //End Frame Secondary 
+
+	 unset($secondary_array); 
+ 
+
+ //End if trigger is true 
+ } 
+
+
+	//end function process_wflow_triggers
+	}
+
+	//end class
+	}
+
+?>
