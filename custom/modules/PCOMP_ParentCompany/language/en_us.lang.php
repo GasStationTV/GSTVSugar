3c953a481ceafab8d9cb43d3028c64c442d713a1
root <starrychloe@oliveyou.net>
2013-10-09 02:14:02 +0000

    Committed and pushed to Git because there were many files that looked like they were changed.

---
+++ custom/modules/PCOMP_ParentCompany/language/en_us.lang.php
@@ -0,0 +1,5 @@
+<?php
+// created: 2013-09-26 12:52:35
+$mod_strings = array (
+  'LBL_EDITVIEW_PANEL1' => 'Assigned To',
+);
---
+++ custom/modules/gpop_PreOpportunity/language/en_us.lang.php
@@ -0,0 +1,12 @@
+<?php
+// created: 2013-09-26 13:23:38
+$mod_strings = array (
+  'LBL_PLANNER1' => 'Primary Planner',
+  'LBL_PLANNER2' => 'Planner',
+  'LBL_PLANNER3' => 'Planner',
+  'LBL_PLANNER4' => 'Planner',
+  'LBL_SELLER1' => 'Primary Seller ',
+  'LBL_SELLER2' => 'Seller',
+  'LBL_SELLER3' => 'Seller',
+  'LBL_SELLER4' => 'Seller',
+);
